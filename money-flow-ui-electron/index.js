ae611ebfeb0024b7f944720904fec27602b19684
Luiz Carlos <Luiz Carlos>
2019-03-28 11:35:49 -0300

    Adicionando a pasta DIST no GIT

--- money-flow-ui-electron/index.js
+++ money-flow-ui-electron/index.js
@@ -1,8 +1,14 @@
 const electron = require('electron');
-const { app, BrowserWindow } = electron;
+
+const { app, BrowserWindow, ipcMain } = electron;
 
 app.on('ready', () => {
     const mainWindow = new BrowserWindow({});
-    mainWindow.setMenu(null);
-    mainWindow.loadFile('./dist/index.html');
+    //mainWindow.setMenu(null);
+    //mainWindow.loadFile('./dist/index.html');
+    mainWindow.loadFile('./test/index.html');
+});
+
+ipcMain.on('myevent', (event, data) => {
+    event.sender.webContents.send('myresponse', 'it worked! ' + data.texto);
 });
